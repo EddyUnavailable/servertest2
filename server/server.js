dd3002e6dc857fa184b9906c12581d6505961f6a
eddy-sullivan <fleatoad@hotmail.com>
2025-02-05 03:08:49 +0000

    9

--- server/server.js
+++ server/server.js
@@ -1,28 +1,35 @@
-import express, { response } from "express";
+// import express, { response, response } from "express";
+import express from "express";
 import cors from "cors";
+
 const app = express();
-app.use(express.json());
-const port = process.env.port || 3000;
+// app.use(express.json());
+app.use(cors());
+const PORT = process.env.port || 3000;
 
-app.listen(port, () => {
-    console.log("server listening on port:", port);
-});
 
-app.get("/status", (request, response) => {
-    const status = {
-        "status": "running"
-    };
+app.listen(PORT, () => console.log(`server is now listening on port${PORT}`));
 
-    response.send(status);
-});
 
-app.get('https://uselessfacts.jsph.pl/random.json', async (req, res) => {
-    // want fetch my jokes from my SQL TABLE
-    // result is a big JSON object that has a bunch of data on it
-    const result = await db.query(`SELECT * FROM jokes`)
-    // we only care about the 'rows' part of the result
-    const jokes = result.rows
-    res.json(jokes)
+const URL = 'https://uselessfacts.jsph.pl/random.json'
+fetch(URL, {
+    category: {
+        "Animals": "Random Animals",
+    },
 })
-    
+.then((resonse) => resonse.json())
+.then(data => {
+    console.log(data)
+})
+  
+// (async() => {
+//     const response = await fetch(`https://quotes.toscrape.com/random`)
+//     const body = await response.text()
+//     console.log(body)
+// })()
 
+//   app.get(URL, (request, response) => {
+//     response.json(URL);
+//   });
+  
+//   app.listen(3000, () => console.log("Woop! App is running in 8080!"));
